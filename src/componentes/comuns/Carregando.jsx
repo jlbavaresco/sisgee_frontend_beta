650ce26db5cbdeefe5c7d1ee26820e5c1a197c73
jlbavaresco@gmail.com <jlbavaresco@gmail.com>
2023-04-04 08:36:42 -0300

    Ajustando o componente carregamento

--- src/componentes/comuns/Carregando.jsx
+++ src/componentes/comuns/Carregando.jsx
@@ -1,10 +1,10 @@
 function Carregando() {
-    return (
-        <div className="d-flex align-items-center">
-        <strong>Carregando...</strong>
-        <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-      </div>
-    )
+  return (
+    <div className="d-flex align-items-center m-5">
+      <strong>Carregando...</strong>
+      <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+    </div>
+  )
 }
 
 export default Carregando;
